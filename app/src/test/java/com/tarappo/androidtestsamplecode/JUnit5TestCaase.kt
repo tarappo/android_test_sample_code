9957edc0a3e32a6d1fd1347d68958aab103bccd5
tarappo <tarappo@gmail.com>
2025-03-16 07:34:56 +0900

    Androidプロジェクトの雛形

---
+++ app/src/test/java/com/tarappo/androidtestsamplecode/JUnit5TestCaase.kt
@@ -0,0 +1,11 @@
+package com.tarappo.androidtestsamplecode
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class JUnit5TestCaase {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
