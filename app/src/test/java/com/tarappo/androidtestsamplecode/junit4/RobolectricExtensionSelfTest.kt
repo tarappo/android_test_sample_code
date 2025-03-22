48c4615883c85644990cde353934ccf9ad059e75
tarappo <tarappo@gmail.com>
2025-03-22 17:22:58 +0900

    JUnit4の場合のテストコードを追加

--- app/src/test/java/com/tarappo/androidtestsamplecode/junit4/RobolectricExtensionSelfTest.kt
+++ app/src/test/java/com/tarappo/androidtestsamplecode/junit4/RobolectricExtensionSelfTest.kt
@@ -1,10 +1,13 @@
 package com.tarappo.androidtestsamplecode.junit4
 
 import android.app.Application
+import android.content.Context
+import com.tarappo.androidtestsamplecode.PreferenceUtil
 import com.tarappo.androidtestsamplecode.ToastUtil
 import org.junit.Before
 import org.junit.Test
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
 import org.junit.Assert.assertTrue
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
@@ -15,8 +18,13 @@ import org.robolectric.shadows.ShadowToast
 @RunWith(RobolectricTestRunner::class)
 @Config(application = RobolectricExtensionSelfTest.MyTestApplication::class)
 class RobolectricExtensionSelfTest {
+    private lateinit var context: Context
+    private lateinit var preferenceUtil: PreferenceUtil
+
     @Before
     fun setUp() {
+        context = RuntimeEnvironment.getApplication()
+        preferenceUtil = PreferenceUtil(context)
     }
 
     @Test
@@ -37,6 +45,20 @@ class RobolectricExtensionSelfTest {
 
         val actualToastMessage = ShadowToast.getTextOfLatestToast()
         assertEquals(expectedMessage, actualToastMessage)
+    }
+
+    @Test
+    fun saveUserName_shouldStoreCorrectValue() {
+        preferenceUtil.saveUserName("サンプル太郎")
+
+        val savedName = preferenceUtil.getUserName()
+        assertEquals("サンプル太郎", savedName)
+    }
+
+    @Test
+    fun getUserName_shouldReturnNullIfNotSet() {
+        val savedName = preferenceUtil.getUserName()
+        assertNull(savedName)
     }
 
     // サンプルコード
